32ca4ddc73018f984e9ac86dc2661e58f5be969c
IRVINGPINEDA <irvingpineda684@gmail.com>
2025-12-03 04:04:54 -0600

    Fix: ENV y db

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'lego_store'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'lego_store',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 connection.connect((err) => {
